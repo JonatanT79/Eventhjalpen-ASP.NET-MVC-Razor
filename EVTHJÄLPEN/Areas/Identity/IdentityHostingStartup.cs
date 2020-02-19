4969daa5958f37a366d1b8e4900fecd09ae1ffb7
Josefine Anerland <jossan-a-h@hotmail.com>
2020-02-19 19:12:24 +0100

    new layout changes

---
+++ EVTHJÄLPEN/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Eventhjälpen.Models;
+using EVTHJÄLPEN.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EVTHJÄLPEN.Areas.Identity.IdentityHostingStartup))]
+namespace EVTHJÄLPEN.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
