16e96cbe9f75fff75cc7cc2d7d465ec4b9442c89
Jonathan Tran <jonathan.t79@hotmail.com>
2020-02-21 11:09:04 +0100

    Changes To Recipe & Event Page

--- EVTHJÄLPEN/Controllers/RecipeController.cs
+++ EVTHJÄLPEN/Controllers/RecipeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Eventhjälpen.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace EVTHJÄLPEN.Controllers
 {
@@ -11,18 +12,35 @@ namespace EVTHJÄLPEN.Controllers
     {
         public IActionResult RecipeIndex()
         {
-            List<Recipe> recipes = new List<Recipe>()
+            List<Recipe> recipes = new List<Recipe>();
+            using (SqlConnection con = new SqlConnection("Server = (localdb)\\MSSQLLocalDB; Database = TranbarDB; Trusted_Connection = True;"))
             {
-                new Recipe() {Id = 1, RecipeName = "Kladdkaka", EstimatedTime = 45},
-                new Recipe() {Id = 2, RecipeName = "Äpplepaj", EstimatedTime = 35},
-                new Recipe() {Id = 3, RecipeName = "Frukttårta", EstimatedTime = 60}
-            };
-            return View(recipes);
+                con.Open();
+                string SQL = @"select r.ID, Recipename, EstimatedTime,rt.RecipeTypeName from recipe r
+                               inner join RecipeType rt on r.RecipeTypeID = rt.ID";
+                SqlCommand cmd = new SqlCommand(SQL, con);
+
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while(rdr.Read())
+                {
+                    string RecipeType = "";
+                    Recipe r = new Recipe();
+                    r.Id = rdr.GetInt32(0);
+                    r.RecipeName = rdr.GetString(1);
+                    r.EstimatedTime = rdr.GetInt32(2);
+                    RecipeType = rdr.GetString(3);
+                    recipes.Add(r);
+                }
+                con.Close();
+            }
+                return View(recipes);
         }
 
         public IActionResult ViewRecipe(int ID)
         {
             Recipe r = new Recipe();
+            r.Id = ID;
             return View(r);
         }
     }
