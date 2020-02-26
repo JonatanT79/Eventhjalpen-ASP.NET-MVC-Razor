c45f95877f461a13a47e62746f1bc0782e40b451
Jonathan Tran <jonathan.t79@hotmail.com>
2020-02-26 20:45:42 +0100

    ViewProducts

--- EVTHJÄLPEN/Controllers/RecipeController.cs
+++ EVTHJÄLPEN/Controllers/RecipeController.cs
@@ -37,18 +37,46 @@ namespace EVTHJÄLPEN.Controllers
         [Route("/[controller]/[action]")]
         public IActionResult ViewRecipe(int id)
         {
-            try
+            List<ViewProducts> Productslist = new List<ViewProducts>();
+            using (SqlConnection con = new SqlConnection("Server=(localdb)\\Mssqllocaldb; Database= TranbarDB; MultipleActiveResultSets=true"))
             {
-                using (ApplicationDbContext ctx = new ApplicationDbContext())
+                con.Open();
+                String SQL = @"select ProductName, Quantity, ProductQuantity,Measurement, RecipeName
+                                from Products PR 
+                                INNER JOIN RecipeDetails RD ON PR.ID = RD.ProductID 
+                                INNER JOIN MeasurementUnit MU ON RD.MeasurementUnitID = MU.Id 
+                                INNER JOIN Recipe RE ON RD.RecipeID = RE.ID  
+                                where RE.Id = @ID";
+
+                SqlCommand cmd = new SqlCommand(SQL, con);
+                cmd.Parameters.AddWithValue("@ID", id);
+
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
                 {
-                    Recipe loadedRecipe = ctx.Recipe.FirstOrDefault(x => x.Id == id);
-                    return View(loadedRecipe);
+                    ViewProducts vp = new ViewProducts();
+                    vp.ProductName = rdr.GetString(0);
+                    vp.ProductQuantity = rdr.GetDecimal(2);
+                    vp.Measurement = rdr.GetString(3);
+                    Productslist.Add(vp);
                 }
+                con.Close();
             }
-            catch (Exception e)
-            {
-                return Content("Failed loading recipe: " + e);
-            }
+            return View(Productslist);
+
+            //try
+            //{
+            //    using (ApplicationDbContext ctx = new ApplicationDbContext())
+            //    {
+            //        Recipe loadedRecipe = ctx.Recipe.FirstOrDefault(x => x.Id == id);
+            //        return View(loadedRecipe);
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    return Content("Failed loading recipe: " + e);
+            //}
         }
     }
 }
