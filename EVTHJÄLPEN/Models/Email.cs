51ad5f87ab2e33ff74ae3cd21aeb812128126d8f
felky <felixg.nti@gmail.com>
2020-03-23 16:35:42 +0100

    Confirm Mail

---
+++ EVTHJÄLPEN/Models/Email.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EVTHJÄLPEN.Models
+{
+    public class Email
+    {
+        public string to { get; set; }
+        public string subj { get; set; }
+        public string body { get; set; }
+    }
+}
