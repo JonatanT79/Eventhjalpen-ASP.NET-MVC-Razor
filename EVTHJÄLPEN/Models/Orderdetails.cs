30c4c7e99548a315cc70eea1387d7a3752147f58
Jonathan Tran <jonathan.t79@hotmail.com>
2020-03-23 20:58:55 +0100

    Database Changes

--- EVTHJÄLPEN/Models/Orderdetails.cs
+++ EVTHJÄLPEN/Models/Orderdetails.cs
@@ -8,6 +8,7 @@ namespace Eventhjälpen.Models
         public int Id { get; set; }
         public int? OrdersId { get; set; }
         public int? ProductId { get; set; }
+        public int? Amount { get; set; }
         public virtual Orders Orders { get; set; }
         public virtual Products Product { get; set; }
     }
