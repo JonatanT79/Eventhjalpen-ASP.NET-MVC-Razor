71b9c15150d9cbb3cd853b70de2e7a3757f68a4c
Jonathan Tran <jonathan.t79@hotmail.com>
2020-03-25 01:46:21 +0100

    Modify Shopping Cart

--- EVTHJÄLPEN/Controllers/HomeController.cs
+++ EVTHJÄLPEN/Controllers/HomeController.cs
@@ -52,15 +52,11 @@ namespace EVTHJÄLPEN.Controllers
             return View();
         }
 
-        public IActionResult Varukorg(int ID, string Empty, int RemoveID, int ProductID, int Amount = 1)
+        public IActionResult Varukorg(int ID, string Empty, int RemoveID, int ProductID, string Apply)
         {
             ViewProducts vp = new ViewProducts();
-            var varukorg = Request.Cookies.SingleOrDefault(c => c.Key == "Varukorg");
 
-            if(Amount <= 0)
-            {
-                Amount = 0;
-            }
+            var varukorg = Request.Cookies.SingleOrDefault(c => c.Key == "Varukorg");
 
             if (RemoveID == 0)
             {
@@ -71,7 +67,7 @@ namespace EVTHJÄLPEN.Controllers
                                             where e.RecipeId == ID
                                             select e.ProductId;
 
-                    if(varukorg.Value != "" && varukorg.Value != null && ID != 0)
+                    if (varukorg.Value != "" && varukorg.Value != null && ID != 0)
                     {
                         cookieString = varukorg.Value + "," + string.Join(",", recipeProductsIds);
                     }
@@ -81,46 +77,153 @@ namespace EVTHJÄLPEN.Controllers
                     }
                     var productIds = cookieString.Split(",").Select(c => int.Parse(c));
 
-                    var products = from e in ctx.Products
-                                   where productIds.Contains(e.Id)
-                                   select e;
+                    var u = from e in ctx.Orderdetails
+                            select e;
 
-                    if (!cookieString.Equals(""))
+                    if (u.Count() == 0)
                     {
-                        foreach (var item in products)
+                        var products = from e in ctx.Products
+                                       where productIds.Contains(e.Id)
+                                       select e;
+
+                        int i = 0;
+                        if (!cookieString.Equals(""))
                         {
-                            ShowIngrediens si = new ShowIngrediens();
-                            si.ProductID = item.Id;
-                            si.ProductName = item.ProductName;
-                            si.Quantity = item.Quantity;
-                            si.Price = item.Price;
+                            foreach (var item in products)
+                            {
+                                Orderdetails od = new Orderdetails();
+                                od.ProductId = item.Id;
+                                od.Amount = 1;
 
-                            if(si.ProductID == ProductID)
-                            {
-                                si.Amount = Amount;
-                            }
-                            else
-                            {
+                                using (ApplicationDbContext c = new ApplicationDbContext())
+                                {
+                                    if (!c.Orderdetails.Any(A => od.ProductId == item.Id))
+                                    {
+                                        ctx.Orderdetails.Add(od);
+                                    }
+
+                                    var modify = from m in c.Orderdetails
+                                                 where m.ProductId == ProductID
+                                                 select m;
+
+                                    if (i == 0 && Apply == "Add")
+                                    {
+                                        foreach (var items in modify)
+                                        {
+                                            items.Amount++;
+                                        }
+                                    }
+                                    else if (i == 0 && Apply == "Remove")
+                                    {
+                                        foreach (var items in modify)
+                                        {
+                                            items.Amount--;
+                                        }
+                                    }
+                                    c.SaveChanges();
+                                }
+
+                                ShowIngrediens si = new ShowIngrediens();
+                                si.ProductID = item.Id;
+                                si.ProductName = item.ProductName;
+                                si.Quantity = item.Quantity;
+                                si.Price = item.Price;
                                 si.Amount = 1;
+                                vp.TotalSum += (decimal.ToDouble(si.Price) * si.Amount);
+                                vp.Productslist.Add(si);
+                                i++;
                             }
+                        }
+                    }
+                    else
+                    {
+                        using (ApplicationDbContext c = new ApplicationDbContext())
+                        {
+                            int i = 0;
+                            var modify = from m in c.Orderdetails
+                                         where m.ProductId == ProductID
+                                         select m;
 
-                            vp.TotalSum += (decimal.ToDouble(si.Price) * si.Amount);
-                            vp.Productslist.Add(si);
+                            if (i == 0 && Apply == "Add")
+                            {
+                                foreach (var items in modify)
+                                {
+                                    items.Amount++;
+                                }
+                            }
+                            else if (i == 0 && Apply == "Remove")
+                            {
+                                foreach (var items in modify)
+                                {
+                                    items.Amount--;
+                                }
+                            }
+                            c.SaveChanges();
+
+                            foreach (var additem in productIds)
+                            {
+                                Orderdetails od = new Orderdetails() { ProductId = additem, Amount = 1 };
+                                if (!c.Orderdetails.Any(A => A.ProductId == additem))
+                                {
+                                    ctx.Orderdetails.Add(od);
+                                    ctx.SaveChanges();
+                                }
+                            }
+                            var products = from e in ctx.Products
+                                           join e2 in ctx.Orderdetails on e.Id equals e2.ProductId
+                                           where productIds.Contains(e.Id)
+                                           select new
+                                           {
+                                               PID = e.Id,
+                                               PNA = e.ProductName,
+                                               Qua = e.Quantity,
+                                               Pri = e.Price,
+                                               Amo = e2.Amount
+                                           };
+
+                            if (!cookieString.Equals(""))
+                            {
+                                foreach (var item in products)
+                                {
+                                    ShowIngrediens si = new ShowIngrediens();
+                                    si.ProductID = item.PID;
+                                    si.ProductName = item.PNA;
+                                    si.Quantity = item.Qua;
+                                    si.Price = item.Pri;
+                                    si.Amount = item.Amo;
+                                    vp.TotalSum += (decimal.ToDouble(si.Price) * si.Amount);
+                                    vp.Productslist.Add(si);
+                                    i++;
+                                }
+                            }
                         }
                     }
                     Response.Cookies.Append("Varukorg", cookieString, new Microsoft.AspNetCore.Http.CookieOptions { Expires = DateTime.Now.AddMinutes(60.0) });
+                    ctx.SaveChanges();
                 }
             }
 
             if (Empty == "Empty")
             {
-                Response.Cookies.Delete("Varukorg");
-                vp.Productslist.Clear();
-                vp.TotalSum = 0;
-                return View(vp);
+                using (ApplicationDbContext ctx = new ApplicationDbContext())
+                {
+
+                    foreach (var item in ctx.Orderdetails)
+                    {
+                        ctx.Orderdetails.Remove(item);
+                    }
+
+                    ctx.SaveChanges();
+                    Response.Cookies.Delete("Varukorg");
+                    vp.Productslist.Clear();
+                    vp.TotalSum = 0;
+
+                    return View(vp);
+                }
             }
             else if (RemoveID != 0)
             {
+                //om man tar bort en produkt, rensa hela tabellen och lägg till nytt, spara modifierings värderna 
                 using (ApplicationDbContext ctx = new ApplicationDbContext())
                 {
                     varukorg = Request.Cookies.SingleOrDefault(s => s.Key == "Varukorg");
@@ -128,7 +231,7 @@ namespace EVTHJÄLPEN.Controllers
                     string value = varukorg.Value;
                     string cookieString = "";
 
-                    int index = value.IndexOf("," + RemoveID.ToString()+ ","); 
+                    int index = value.IndexOf("," + RemoveID.ToString() + ",");
 
                     if (index == -1)
                     {
@@ -180,13 +283,12 @@ namespace EVTHJÄLPEN.Controllers
                         si.ProductName = item.ProductName;
                         si.Quantity = item.Quantity;
                         si.Price = item.Price;
-                        si.Amount = Amount;
+                        //   si.Amount = Amount;
                         vp.TotalSum += (decimal.ToDouble(si.Price) * si.Amount);
                         vp.Productslist.Add(si);
                     }
                     Response.Cookies.Append("Varukorg", cookieString, new Microsoft.AspNetCore.Http.CookieOptions { Expires = DateTime.Now.AddMinutes(60.0) });
                 }
-
                 return View(vp);
             }
 
